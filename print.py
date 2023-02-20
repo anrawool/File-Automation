052c2e17ad1126799bbb3f1da1e47a7b29302697
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-02-20 22:46:08 +0530

    Print Functiona and Setup Base

--- print.py
+++ print.py
@@ -1,9 +1,23 @@
 from tracker import track
 import os
 
+def sterilize(path):
+    os.chdir("/Users/abhijitrawool/Documents/Print/")
+    ext = os.path.splitext(path)
+    temp_path = path.replace(ext[1], "")
+    new_path = temp_path.replace(".", "_")
+    final_path = new_path + ext[1]
+    final_path = final_path.replace(" ", '_')
+    rename_path = temp_path+ext[1]
+    os.rename(rename_path, final_path)
+    return final_path
 def created(event):
     try:
-        os.system("lp {event.src_path}")
-    except Exception:
+        path = sterilize(event.src_path)
+        final = path.replace("/Users/abhijitrawool/Documents/Print/", "")
+        os.chdir("/Users/abhijitrawool/Documents/Print/")
+        os.system(f"lpr {final}")
+    except Exception as e:
         print("The Printer May Be Offline...")
+        print(e)
 track("/Users/abhijitrawool/Documents/Print", created_func=created)
