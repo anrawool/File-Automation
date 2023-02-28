02af978eecc920442df9b7e4d84f4bca4adccce0
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-02-28 15:22:11 +0530

    Search Function Organised, Ready For Deployment

--- search.py
+++ search.py
@@ -4,13 +4,6 @@ from data import *
 import re
 import time 
 
-mode = ''
-folder = argv[1]
-try:
-    target = argv[2]
-except Exception:
-    mode = 'folder'
-
 class FileSearch():
     def __init__(self, folder, target='', mode=None):
         self.mode = mode
@@ -28,41 +21,13 @@ class FileSearch():
             self.func = "name"
         self.control = False
         self.results = []
+
+    # Sterilization and Trimming Functions
+
     def folder_sterilize(self, folder):
         folder = re.sub('_+', ' ', folder)
         return folder.lower()
-    
-    def name(self, folder_loc, folder_obj):
-        final_files_name = []
-        folder_objs = []
-        try:
-            folder_files = os.scandir(f'{folder_loc}/{self.target_folder}')
-            for i in folder_files:
-                if self.target in i.name.lower():
-                    folder_obj.file = i.name.replace(os.path.splitext(i.name)[1], '')
-                    folder_obj.ext = os.path.splitext(i.name)[1]
-                    folder_objs.append(folder_obj)
-                    final_files_name.append(i.name)
-        except FileNotFoundError:
-            pass
-        return (final_files_name, folder_objs)
-    
-    def ext(self, folder_loc, folder_obj):
-        final_files_ext = []
-        folder_objs = []
-        folder_files = os.scandir(f"{folder_loc}/{self.target_folder}")
-        try:
-            for i in folder_files:
-                if i.name.endswith(target):
-                    folder_obj.file = i.name.replace(os.path.splitext(i.name)[1], '')
-                    folder_obj.ext = os.path.splitext(i.name)[1]
-                    # print(f"Target Found: {i.name}")
-                    folder_objs.append(folder_obj)
-                    final_files_ext.append(i.name)
-        except FileNotFoundError:
-            pass
-        return (final_files_ext, folder_objs)
-    
+
     def folder_trim(self, fold_list):
         self.temp_folds = []
         self.ignore_folds = ['Public', 'Movies', 'Applications', 'opt', 'Library', 'Desktop', 'Pictures', 'Music', 'Sites']
@@ -83,6 +48,75 @@ class FileSearch():
                 pass
         return self.temp_files
 
+    def parent_folder_trim(self):
+        self.sterilized = []
+        for folder in self.parent_folders:
+            self.sterilized.append(folder.lower())
+        if self.target_folder in self.sterilized:
+            self.target_folder = ''
+            return self.target_folder
+        else:
+            return self.target_folder
+
+    # Search Target Functions
+
+    def name(self, folder_loc, folder_obj):
+        final_files_name = []
+        folder_objs = []
+        self.target_folder = self.parent_folder_trim()
+        try:
+            folder_files = os.scandir(f'{folder_loc}/{self.target_folder}')
+            for i in folder_files:
+                if self.target in i.name.lower():
+                    folder_obj.file = i.name.replace(os.path.splitext(i.name)[1], '')
+                    folder_obj.ext = os.path.splitext(i.name)[1]
+                    folder_objs.append(folder_obj)
+                    final_files_name.append(i.name)
+        except FileNotFoundError:
+            pass
+        return (final_files_name, folder_objs)
+    
+    def ext(self, folder_loc, folder_obj):
+        final_files_ext = []
+        folder_objs = []
+        self.target_folder = self.parent_folder_trim()
+        folder_files = os.scandir(f"{folder_loc}/{self.target_folder}")
+        for i in folder_files:
+            if i.name.endswith(target):
+                folder_obj.file = i.name.replace(os.path.splitext(i.name)[1], '')
+                folder_obj.ext = os.path.splitext(i.name)[1]
+                folder_objs.append(folder_obj)
+                final_files_ext.append(i.name)
+        return (final_files_ext, folder_objs)
+    
+    def all_folders(self, folder_obj):
+        final_folder_objs_all = []
+        self.target_folder = self.parent_folder_trim()
+        items = os.scandir(f"{folder_obj.path}/{self.target_folder}")
+        final_folders_all = [folder for folder in items]
+        final_folders_all = self.folder_trim(final_folders_all)
+        for folder in final_folders_all:
+            set_path = f'{folder_obj.path}/{self.target_folder}/{folder}'
+            set_path = re.sub("/+", '/', set_path)
+            folder = self.folder_sterilize(folder=folder)
+            folder_object = self.DataMakerObj.make_folder_path(path=set_path)
+            final_folder_objs_all.append(folder_object)
+        return (final_folders_all, final_folder_objs_all)
+
+    def all_files(self, folder_obj):
+        final_files_objs_all = []
+        self.target_folder = self.parent_folder_trim()
+        items = os.scandir(f"{folder_obj.path}/{self.target_folder}")
+        final_files_all = [file for file in items]
+        final_files_all = self.file_trim(final_files_all)
+        for file in final_files_all:
+            set_path = f'{folder_obj.path}/{file}'
+            set_path = re.sub("/+", '/', set_path)
+            folder_object = self.DataMakerObj.make_path(path=set_path)
+            final_files_objs_all.append(folder_object)
+        return (final_files_all, final_files_objs_all)
+
+    # Folder Search Function
 
     def search_folder(self):
         self.folders = os.scandir("/Users/abhijitrawool/")
@@ -115,26 +149,7 @@ class FileSearch():
                 self.control = True
         return self.results
 
-    def all_folders(self, folder_obj):
-        final_folder_objs_all = []
-        items = os.scandir(f"{folder_obj.path}/{self.target_folder}")
-        final_folders_all = [folder for folder in items]
-        final_folders_all = self.folder_trim(final_folders_all)
-        for folder in final_folders_all:
-            folder = self.folder_sterilize(folder=folder)
-            folder_object = self.DataMakerObj.make_folder_path(path=f'{folder_obj.path}/{self.target_folder}/{folder}')
-            final_folder_objs_all.append(folder_object)
-        return (final_folders_all, final_folder_objs_all)
 
-    def all_files(self, folder_obj):
-        final_files_objs_all = []
-        items = os.scandir(f"{folder_obj.path}/{self.target_folder}")
-        final_files_all = [file for file in items]
-        final_files_all = self.file_trim(final_files_all)
-        for file in final_files_all:
-            folder_object = self.DataMakerObj.make_path(path=f'{folder_obj.path}/{file}')
-            final_files_objs_all.append(folder_object)
-        return (final_files_all, final_files_objs_all)
 
     def search(self):
         result_files = []
@@ -146,32 +161,30 @@ class FileSearch():
         for result in results:
             if self.func == 'name':
                 (files, folder_obj) = self.name(folder_loc=result.path, folder_obj=result)
-                if files != []:
-                    result_files.append(files)
-                    result_folder_objs.append(folder_obj)
             elif self.func == 'all_folders':
                 (files, folder_obj) = self.all_folders(folder_obj=result)
-                if files != []:
-                    result_files.append(files)
-                    result_folder_objs.append(folder_obj)
             elif self.func == 'all_files':
                 (files, folder_obj) = self.all_files(folder_obj=result)
-                if files != []:
-                    result_files.append(files)
-                    result_folder_objs.append(folder_obj)
             else:
                 (files, folder_obj) = self.ext(folder_loc=result.path, folder_obj=result)
-                if files != []:
-                    result_files.append(files)
-                    result_folder_objs.append(folder_obj)
+            if files != []:
+                result_files.append(files)
+                result_folder_objs.append(folder_obj)
         return (result_files, result_folder_objs)
 
-if mode != 'folder':
-    search_method = FileSearch(folder, target)
-    (files_list, folds_list) = search_method.search()
-    print("Final Files List:", files_list)
-    print("\nFinal Folders List:", folds_list)
-else:
-    search_method = FileSearch(folder, mode=mode)
-    folder_loc = search_method.search()
-    print("Folder Location is:", folder_loc)
+if __name__ == '__main__':
+    mode = ''
+    folder = argv[1]
+    try:
+        target = argv[2]
+    except Exception:
+        mode = 'folder'
+    if mode != 'folder':
+        search_method = FileSearch(folder, target)
+        (files_list, folds_list) = search_method.search()
+        print("Final Files List:", files_list)
+        print("\nFinal Folders List:", folds_list)
+    else:
+        search_method = FileSearch(folder, mode=mode)
+        folder_loc = search_method.search()
+        print("Folder Location is:", folder_loc)
