58397dfd9953198c6af26bc13fc8d97b630816ab
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-05-02 14:25:21 +0530

    Basic Merge Functionality Added...

--- merge.py
+++ merge.py
@@ -37,21 +37,32 @@ class RepoMerger:
         self.repo_obj = github.get_repo(f"{Github_User}/{repository}")
         repo = self.repo_obj.full_name
         self.repo_name = repo.replace(f'{Github_User}/', "")
-        self.merge_branches()
+        self.head = self.get_head()
+        self.pull_details = self.create_pull_request("Merge Script In Development", "This is a test to check if merge script is up and running...")
+        self.merge_branches(self.pull_details[2])
     
-    def merge_branches(self):
+    def create_pull_request(self, title, body=''):
+        base = self.repo_obj.get_branch(self.merge_to_branch).name
+        self.repo_obj.create_pull(title=title, body=body, head=self.head, base=base)
+        return self.head, base, title, body
+
+    def get_head(self) -> str:
         if self.merge_branch == 'head':
             command_out = os.popen('git branch').read()
             results = command_out.split('\n')
             for result in results:
                 if "*" in result:
                     self.merge_branch = result.replace("* ", "")
-                    break
+                    return self.merge_branch
                 else:
                     pass
-        base = self.repo_obj.get_branch(self.merge_to_branch)
+
+    def merge_branches(self, title):
+        base = self.repo_obj.get_branch(self.merge_to_branch).name
         head = self.repo_obj.get_branch(self.merge_branch)
-        merge_to_master = self.repo_obj.merge(base, head.commit.sha, "merge to master")
+        print(base)
+        print(head)
+        merge_to_master = self.repo_obj.merge(base, head.commit.sha, title + ' [Merge] ')
         print(merge_to_master)
 
 RepoMerger(repo_name, merge_branch, merge_to_branch)
