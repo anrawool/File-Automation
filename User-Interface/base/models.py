0544cc14e0e8f787d1625f9111a518b8441896cb
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-05-29 22:26:27 +0530

    Upload page UI

--- User-Interface/base/models.py
+++ User-Interface/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.http import JsonResponse
 
 # Create your models here
 
@@ -24,3 +25,16 @@ class Service(models.Model):
 class File(models.Model):
     name = models.CharField(max_length=200, null=True)
     file = models.FileField(upload_to='uploads/')        
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES:
+        uploaded_file = request.FILES['file']
+        
+        # Create a new instance of the model
+        my_file = File()
+        my_file.file.save(uploaded_file.name, uploaded_file)
+        my_file.save()
+        
+        return JsonResponse({'message': 'File uploaded and saved successfully'})
+    
+    return JsonResponse({'message': 'No file found or invalid request'})
