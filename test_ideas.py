551132b2c596a4653df5bd0f520cbce5fd1b7652
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-05-30 21:56:58 +0530

    Upload page UI

--- test_ideas.py
+++ test_ideas.py
@@ -1,15 +1,8 @@
 # File for experimentation
-import encrypter
 
-encrypter_AEA = encrypter.AEA()
-with open('test_file.txt', 'r') as file:
-    text = file.read()
+import datetime
 
-extract_key = encrypter_AEA.use_key('encrypter_key.json')
-if extract_key == encrypter_AEA.final_key:
-    pass
-for i in range(0, 1):
-    max_characters = len(extract_key[i])
-decrypted = encrypter_AEA.decrypt_text(text)
-encrypter_AEA.encrypt_file('./', 'data', '.py')
-encrypter_AEA.decrypt_file('./', 'data_encrypted', '.py')
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+print(today)
+print(tomorrow)
