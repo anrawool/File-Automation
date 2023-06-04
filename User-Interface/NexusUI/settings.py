8c192f3a2ad13f483a19ab887ffce978f03c17f0
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2023-06-04 21:37:56 +0530

    Folder Reorganization

--- User-Interface/NexusUI/settings.py
+++ User-Interface/NexusUI/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'NexusUI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'databases' / 'db.sqlite3',
     }
 }
 
