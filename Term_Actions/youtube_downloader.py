8bfe19653eb54d50d9f2e3f6424852fd17f3d07f
Sarthak Abhijit Rawool <sarthakrawool09@gmail.com>
2023-10-11 22:06:50 +0530

    Youtube Downloader Ready

---
+++ Term_Actions/youtube_downloader.py
@@ -0,0 +1,55 @@
+# File for experimentation
+import __meta
+from pytube import YouTube
+from settings import get_shell_input, ROOT_DIR
+from sys import argv
+import __meta
+
+url, mode, save_path = get_shell_input(1, argv, exceptions=['-v', None]) 
+
+class YoutubeDownloader:
+    def __init__(self, url, mode, save_path):
+        self.video_url = url
+        self.save_path = save_path
+        if ROOT_DIR not in self.save_path:
+            self.save_path = ROOT_DIR + '/' + self.save_path
+        try:
+            self.yt = YouTube(self.video_url)
+        except Exception:
+            raise Exception("This link does not exist on YouTube")
+        self.mode = mode
+        try:
+            if self.mode == '-v':
+                stream = self.download_video()
+            elif self.mode == '-m':
+                stream = self.download_video(audio=False)
+            elif self.mode == '-a':
+                stream = self.download_audio()
+            else:
+                raise Exception("Mode not available")
+        except Exception as e:
+            print(e)
+            raise Exception("The inputs given are invalid")
+        stream.download(output_path=self.save_path)
+
+    def download_video(self, audio=True):
+        if audio:
+            stream = self.yt.streams.get_highest_resolution()
+        else:
+            stream = self.yt.streams.filter(only_video=True).first()
+        return stream
+
+    def download_audio(self):
+        audio_stream = self.yt.streams.filter(only_audio=True).first()
+        return audio_stream
+
+if __name__ == "__main__":
+    if save_path == None and mode == '-v':
+        save_path =f'{__meta.absolute_current_path}../downloads/videos'
+    elif save_path == None and mode == '-a':
+        save_path =f'{__meta.absolute_current_path}../downloads/audio'
+    elif save_path == None and mode == '-m':
+        save_path =f'{__meta.absolute_current_path}../downloads/videos/only_video'
+    else:
+        save_path =f'{__meta.absolute_current_path}../downloads/videos'
+    downloader = YoutubeDownloader(url, mode, save_path)
--- test_ideas.py
+++ test_ideas.py
@@ -1,1 +1,34 @@
-# File for experimentation
+import pyaudio
+import numpy as np
+
+# Initialize PyAudio and audio stream
+p = pyaudio.PyAudio()
+stream = p.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=44100,
+    input=True,
+    frames_per_buffer=1024
+)
+
+try:
+    while True:
+        # Read audio data from the stream
+        data = stream.read(1024)
+        # Convert the binary audio data to a NumPy array
+        audio_signal = np.frombuffer(data, dtype=np.int16)
+        # Calculate the root mean square (RMS) of the audio signal
+        rms = np.sqrt(np.mean(np.square(audio_signal)))
+        # Convert RMS to decibels (dB)
+        db = 20 * np.log10(rms)
+
+        print(f"Decibel level: {db:.2f} dB")
+
+except KeyboardInterrupt:
+    pass
+
+# Close the audio stream and terminate PyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
+
