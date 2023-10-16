0137b275fcbbc6bb539d32d6e696c63a6f64deb7
Sarthak Abhijit Rawool <sarthakrawool09@gmail.com>
2023-10-16 23:48:21 +0530

    Additional Changes

--- User-Interface/NexusUI/settings.py
+++ User-Interface/NexusUI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*nuy^)^t7mm^yl2as!1jy+g-#+te70#=$v&73q#e@9&$8b^%m9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.58', '127.0.0.1']
 
