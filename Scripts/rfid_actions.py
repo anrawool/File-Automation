b4243ec00d6563ec6eac39bd8477668649c15440
anrawool <anrawool@gmail.com>
2023-11-11 16:14:57 +0530

    Server Changes

--- Scripts/rfid_actions.py
+++ Scripts/rfid_actions.py
@@ -1,6 +1,6 @@
 import __meta
 from Controllers.rfid_controller import * 
-from Controller import server_conn
+from Controllers import server_conn
 
 class RFID_Action:
 
@@ -9,8 +9,7 @@ class RFID_Action:
     
     def home_action_selector(self):
         self.id, self.desc = self.instance.read_card()
-
-        if self.desc == "School":
+        if self.id == 771040611745:
             print("Welcome Back Sir!!!")
             # Scrape FIITJEE Stuff
             connection = server_conn.SSH_Connection('abhijitrawool', '192.168.1.13')
@@ -18,3 +17,5 @@ class RFID_Action:
             connection.close_channel()
             
 
+action = RFID_Action()
+action.home_action_selector()
