377159a2c5bba52e210017204adaa5311b31bb0b
Sarthak Abhijit Rawool <sarthakrawool09@gmail.com>
2023-11-12 16:44:17 +0530

    RFID Actions

--- Scripts/rfid_actions.py
+++ Scripts/rfid_actions.py
@@ -9,11 +9,15 @@ class RFID_Action:
     
     def home_action_selector(self):
         self.id, self.desc = self.instance.read_card()
+        print(self.desc)
         if self.id == 771040611745:
             print("Welcome Back Sir!!!")
             # Scrape FIITJEE Stuff
             connection = server_conn.SSH_Connection('abhijitrawool', '192.168.1.13')
-            final = connection.exec_command("cd Documents && mkdir New")
+            _ = connection.exec_command("open -a Firefox")
+            _ = connection.exec_command("open -a \"Visual Studio Code.app\"")
+            _ = connection.exec_command("open -a iTerm.app")
+
             connection.close_channel()
             
 
